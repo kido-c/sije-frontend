b11fed3e8da3e8a9bc1743285ff090c44d278f89
Ki Do <65141066+kido-c@users.noreply.github.com>
2024-09-22 14:40:41 +0900

    style: theme 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import GlobalStyles from "./styles/globalStyle";
 import GlobalFonts from "./styles/globalFont.ts";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GlobalStyles />
-    <GlobalFonts />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <GlobalFonts />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );

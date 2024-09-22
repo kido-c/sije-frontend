5d085c989ba9bb42a949d1bd82dde6ff104b5840
Ki Do <65141066+kido-c@users.noreply.github.com>
2024-09-22 13:40:29 +0900

    style: reset style, global style setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import GlobalStyles from "./styles/globalStyle";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

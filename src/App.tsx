5f8079e5264f5bbd0c286c5761ec164d0e580652
Ki Do <65141066+kido-c@users.noreply.github.com>
2024-09-22 23:40:15 +0900

    Feat/gallery list (#4)
    * feat: add gallery item
    
    * feat: add gallery list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import { styled } from "styled-components";
 import Header from "./components/Header";
+import GalleryList from "./components/gallery/GalleryList";
+
+const IMG_SOURCE = [
+  {
+    src: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+    alt: "A beautiful mountain landscape",
+    title: "Mountain View",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2016/11/29/09/05/architecture-1867187_1280.jpg",
+    alt: "Modern city skyline",
+    title: "City Skyline",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1499346030926-9a72daac6c63",
+    alt: "Calm ocean view",
+    title: "Ocean at Sunset",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2569234_1280.jpg",
+    alt: "People walking on the street",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1531177075029-becfd7f4e818",
+    alt: "A serene forest",
+    title: "Forest Path",
+  },
+];
 
 function App() {
   return (
     <Container>
       <Header />
-      <Warpper>content</Warpper>
+      <Warpper>
+        <GalleryList galleryList={IMG_SOURCE} />
+      </Warpper>
     </Container>
   );
 }
---
+++ src/components/gallery/GalleryList.tsx
@@ -0,0 +1,31 @@
+import GalleryItem from "./GalleryItem";
+import { styled } from "styled-components";
+
+interface Props {
+  galleryList: {
+    src: string;
+    alt: string;
+    title?: string;
+    width?: number;
+    height?: number;
+  }[];
+}
+
+export default function GalleryList({ galleryList }: Props) {
+  return (
+    <Container>
+      {galleryList.map((gallery) => (
+        <GalleryItem {...gallery} key={gallery.title} />
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  row-gap: 10px;
+  column-gap: 20px;
+`;

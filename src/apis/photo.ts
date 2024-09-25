4763751a430623d323c4a3d5bfa3670d73fa3970
Ki Do <65141066+kido-c@users.noreply.github.com>
2024-09-25 11:57:41 +0900

    Feat/loading spinder (#15)
    * feat: 초기 랜더링 로딩 스피너 적용
    
    * fix: build error

--- src/apis/photo.ts
+++ src/apis/photo.ts
@@ -5,15 +5,24 @@ const baseURL = "https://api.unsplash.com/";
 
 export const PhotoAPI = {
   getRandomPhotos: async ({
+    page,
     count,
   }: {
+    page: number;
     count: number;
   }): Promise<GetPhotoDTO[]> => {
-    const { data } = await axios.get(
+    const { data, status } = await axios.get(
       `${baseURL}/photos/random?client_id=${
         import.meta.env.VITE_UPSPLASH_ACC_KEY
-      }&count=${count}`
+      }&count=${count}&page=${page}`
     );
-    return data;
+
+    if (status === 403) {
+      throw new Error("하루 요청량을 초과하였습니다.");
+    } else if (status !== 200) {
+      throw new Error("요청에 실패하였습니다.");
+    } else {
+      return data;
+    }
   },
 };
--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,21 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme.ts";
 import PortalModal from "./components/modal/components/PortalModal.tsx";
 import { ModalProvider } from "./contexts/ModalProvider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ModalProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <GlobalFonts />
-        <PortalModal />
-        <App />
-      </ThemeProvider>
-    </ModalProvider>
+    <QueryClientProvider client={queryClient}>
+      <ModalProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <GlobalFonts />
+          <PortalModal />
+          <App />
+        </ThemeProvider>
+      </ModalProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
